189089e4fd700bff353bfc90477825e12e3dd066
Adam James <11ahj11@gmail.com>
2024-12-06 13:36:23 -0500

    style changes

--- about.php
+++ about.php
@@ -4,37 +4,37 @@ include 'header.php';
 
 <div class="main-wrapper">
     <div class="m-3 m-lg-5">
+        <h1 class="fs-1 px-0 px-lg-5  mb-0 mb-lg-4">Our Story</h1>
         <div class="row">
-            <div class="my-4 my-lg-0 col-lg">
-                <h1 class="fs-1">Our Story</h1>
-                <p class="fs-6">Welcome to Taj, where the heart of Indian cuisine meets a warm, family-driven atmosphere. Founded by Sai Guntaka and his family in 2012, Taj began as a modest venture in South Portland. Despite early struggles, the Guntakas' dedication, resilience, and love for their craft turned their dream into a beloved local gem.</p>
+            <div class="col-lg d-none d-lg-block">
+                <div>
+                    <img src="/assets/images/taj-about-family.png" class="img-fluid px-5" alt="The Reddy Guntaka family: Sai, Charitha, Geetansh, Sudarsana, Sidhansh and Hemalatha.">
+                    <p class="tiny-text px-5 mb-0">The Reddy Guntaka family (from left): Sai, Charitha, Geetansh, Sudarsana, Sidhansh and Hemalatha.</p>
+                    <p class="tiny-text px-5 mb-0">(photo credit: Kiki Garfield from The Bollard)</p>
+                </div>
+                <div>
+                    <img src="/assets/images/taj-about-bar.png" class="img-fluid pt-5 px-5" alt="Sai Guntaka with Jim Baldi at Taj's new bar">
+                    <p class="tiny-text px-5 mb-0">Sai Guntaka (left) with Jim Baldi at Taj's new bar.</p>
+                    <p class="tiny-text px-5 mb-0">(photo credit: Kiki Garfield from The Bollard)</p>
+                </div>
+            </div>
+            <div class="my-2 my-lg-0 col-lg">
+                <p class="about-text">Welcome to Taj, where the heart of Indian cuisine meets a warm, family-driven atmosphere. Founded by Sai Guntaka and his family in 2012, Taj began as a modest venture in South Portland. Despite early struggles, the Guntakas' dedication, resilience, and love for their craft turned their dream into a beloved local gem.</p>
                 <div class="d-block d-lg-none">
                     <img src="/assets/images/taj-about-family.png" class="img-fluid" alt="The Reddy Guntaka family: Sai, Charitha, Geetansh, Sudarsana, Sidhansh and Hemalatha.">
                     <p class="tiny-text mb-0">The Reddy Guntaka family (from left): Sai, Charitha, Geetansh, Sudarsana, Sidhansh and Hemalatha.</p>
                     <p class="tiny-text">(photo credit: Kiki Garfield from The Bollard)</p>
                 </div>
-                <p class="fs-6">At the core of Taj is Sai's mother, Chef Hemalatha Reddy Guntaka, whose mastery of traditional Indian spices forms the foundation of every dish. The family takes immense pride in the authenticity of their food, which is made fresh daily with ingredients sourced directly from India. From their iconic lamb vindaloo to their handmade garlic naan, every item on the menu is crafted with care and passion.</p>
-                <p class="fs-6">Taj is not just a restaurant; it's a reflection of the Guntakas' commitment to community and kindness. They're dedicated to giving back, often donating leftover food to local shelters and providing meals for those in need. This spirit of generosity extends to their work environment as well, where family and dedicated staff work side by side, ensuring a welcoming atmosphere for all.</p>
+                <p class="about-text">At the core of Taj is Sai's mother, Chef Hemalatha Reddy Guntaka, whose mastery of traditional Indian spices forms the foundation of every dish. The family takes immense pride in the authenticity of their food, which is made fresh daily with ingredients sourced directly from India. From their iconic lamb vindaloo to their handmade garlic naan, every item on the menu is crafted with care and passion.</p>
+                <p class="about-text">Taj is not just a restaurant; it's a reflection of the Guntakas' commitment to community and kindness. They're dedicated to giving back, often donating leftover food to local shelters and providing meals for those in need. This spirit of generosity extends to their work environment as well, where family and dedicated staff work side by side, ensuring a welcoming atmosphere for all.</p>
 
-                <p class="fs-6">In 2024, Taj expanded to a stunning new location, tripling its size and bringing an even better dining experience. The new space includes a bar, where you can enjoy cocktails inspired by Indian flavors, alongside the same delicious dishes that made Taj a Portland favorite.</p>
+                <p class="about-text">In 2024, Taj expanded to a stunning new location, tripling its size and bringing an even better dining experience. The new space includes a bar, where you can enjoy cocktails inspired by Indian flavors, alongside the same delicious dishes that made Taj a Portland favorite.</p>
                 <div class="d-block d-lg-none">
                     <img src="/assets/images/taj-about-bar.png" class="img-fluid" alt="Sai Guntaka with Jim Baldi at Taj's new bar">
                     <p class="tiny-text mb-0">Sai Guntaka (left) with Jim Baldi at Taj's new bar.</p>
                     <p class="tiny-text">(photo credit: Kiki Garfield from The Bollard)</p>
                 </div>
-                <p class="fs-6">Join us at Taj to experience not just a meal, but a story of perseverance, tradition, and community. We can't wait to welcome you.</p>
-            </div>
-            <div class="col-lg">
-                <div class="d-none d-lg-block">
-                    <img src="/assets/images/taj-about-family.png" class="img-fluid px-5" alt="The Reddy Guntaka family: Sai, Charitha, Geetansh, Sudarsana, Sidhansh and Hemalatha.">
-                    <p class="tiny-text px-5 mb-0">The Reddy Guntaka family (from left): Sai, Charitha, Geetansh, Sudarsana, Sidhansh and Hemalatha.</p>
-                    <p class="tiny-text px-5 mb-0">(photo credit: Kiki Garfield from The Bollard)</p>
-                </div>
-                <div class="d-none d-lg-block">
-                    <img src="/assets/images/taj-about-bar.png" class="img-fluid pt-5 px-5" alt="Sai Guntaka with Jim Baldi at Taj's new bar">
-                    <p class="tiny-text px-5 mb-0">Sai Guntaka (left) with Jim Baldi at Taj's new bar.</p>
-                    <p class="tiny-text px-5 mb-0">(photo credit: Kiki Garfield from The Bollard)</p>
-                </div>
+                <p class="about-text">Join us at Taj to experience not just a meal, but a story of perseverance, tradition, and community. We can't wait to welcome you.</p>
             </div>
         </div>
     </div>
