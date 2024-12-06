0e7e978d41f2ec7edc2f39c58fb91dea86402b71
Adam James <11ahj11@gmail.com>
2024-12-06 15:45:17 -0500

    building out info in contact page

--- contact.php
+++ contact.php
@@ -3,8 +3,41 @@ include 'header.php';
 ?>
 
 <div class="main-wrapper">
-    <h1>Contact Page</h1>
-
-<?php
+    <div class="m-3 m-lg-5">
+        <div class="container">
+            <div class="d-flex flex-column">
+                <h1>Contact Us</h1>
+                <a class="mb-1 href="tel:+12078286677">(207) 828-6677</a>
+                <a href="mailto:contact@tajofmaine.com">contact@tajofmaine.com</a>
+            </div>
+            <div class="d-flex flex-column mt-4">
+                <h2>Address</h2>
+                <p class="mb-2">333 Clarks Pond PKWY</p>
+                <p class="mb-2">Ste #6</p>
+                <p class="mb-2">South Portland, ME 04106</p>
+            </div>
+            <div class="d-flex flex-column mt-4">
+                <h2>Hours</h2>
+                <p class="mb-2">Lunch Mon - Fri: 11:00am - 2:30pm</p>
+                <p class="mb-2">Dinner Mon - Thu: 4:30pm - 8:45pm</p>
+                <ul class="mb-2">
+                    <li class="tiny-text">Bar until 10:00pm</li>
+                </ul>
+                <p class="mb-2">Dinner Fri: 4:30pm - 9:45pm</p>
+                <ul class="mb-2">
+                    <li class="tiny-text">Bar until 11:00pm</li>
+                </ul>
+                <p class="mb-2">Sat: 11:00am - 9:45pm</p>
+                <ul class="mb-2">
+                    <li class="tiny-text">Bar until 11:00pm</li>
+                </ul>
+                <p class="mb-2">Sun: 11:00am - 8:45pm</p>
+                <ul class="mb-2">
+                    <li class="tiny-text">Bar until 11:00pm</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <?php
     include 'footer.php';
     ?>
