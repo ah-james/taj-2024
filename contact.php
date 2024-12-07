e8a538b90b9fc4e3b27c37b6212fa05dd15d39d4
Adam James <11ahj11@gmail.com>
2024-12-07 13:48:12 -0500

    styling changes

--- contact.php
+++ contact.php
@@ -3,21 +3,21 @@ include 'header.php';
 ?>
 
 <div class="main-wrapper">
-    <div class="m-3 m-lg-5">
-        <div class="container">
+    <div class="row m-3 m-lg-5">
+        <div class="col-lg-3">
             <div class="d-flex flex-column">
-                <h1>Contact Us</h1>
-                <a class="mb-1 href="tel:+12078286677">(207) 828-6677</a>
+                <h1 class="my-3 my-lg-0 mb-lg-3">Contact Us</h1>
+                <a class="mb-2" href="tel:+12078286677">(207) 828-6677</a>
                 <a href="mailto:contact@tajofmaine.com">contact@tajofmaine.com</a>
             </div>
             <div class="d-flex flex-column mt-4">
-                <h2>Address</h2>
+                <h3>Address</h3>
                 <p class="mb-2">333 Clarks Pond PKWY</p>
                 <p class="mb-2">Ste #6</p>
                 <p class="mb-2">South Portland, ME 04106</p>
             </div>
             <div class="d-flex flex-column mt-4">
-                <h2>Hours</h2>
+                <h3>Hours</h3>
                 <p class="mb-2">Lunch Mon - Fri: 11:00am - 2:30pm</p>
                 <p class="mb-2">Dinner Mon - Thu: 4:30pm - 8:45pm</p>
                 <ul class="mb-2">
@@ -36,6 +36,9 @@ include 'header.php';
                     <li class="tiny-text">Bar until 11:00pm</li>
                 </ul>
             </div>
+        </div>
+        <div class="col-lg mt-3 mt-lg-0">
+            <img src="/assets/images/taj-about-bar.png" class="img-fluid" alt="Sai Guntaka with Jim Baldi at Taj's new bar">
         </div>
     </div>
     <?php
